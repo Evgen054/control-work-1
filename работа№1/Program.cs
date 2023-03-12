c398750999f0c5e0802d56500cfe9647ed46db3c
Evgen Dirt <bankir032@mail.ru>
2023-03-12 15:00:10 +0300

    ввел первоначальный массив с клавиатуры

--- работа№1/Program.cs
+++ работа№1/Program.cs
@@ -25,3 +25,6 @@ void PrintArray(string[] array)
     Console.WriteLine();
 }
 
+string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+
