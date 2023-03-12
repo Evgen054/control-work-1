77495c027704dfe7dd63c4d54fefb938e2235984
Evgen Dirt <bankir032@mail.ru>
2023-03-12 15:41:08 +0300

    добавил файл READMI.md с описанием решения задачи

--- работа№1/Program.cs
+++ работа№1/Program.cs
@@ -8,25 +8,41 @@ void SecondArrayWithIF(string[] array1, string[] array2)
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
+        if (array1[i].Length <= 3)
         {
-        array2[count] = array1[i];
-        count++;
+            array2[count] = array1[i];
+            count++;
         }
     }
+
 }
 
 void PrintArray(string[] array)
 {
+
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} ");
+        Console.Write($"{array[i]}");
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
     }
-    Console.WriteLine();
+
 }
 
-string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+
+
+string[] array1 = new string[5] { "123", "23", "hello", "world", "res" };
 string[] array2 = new string[array1.Length];
 
 SecondArrayWithIF(array1, array2);
+Console.Write($"[");
+PrintArray(array1);
+Console.Write($"]");
+
+Console.Write(" -> ");
+
+Console.Write($"[");
 PrintArray(array2);
+Console.Write($"]");
